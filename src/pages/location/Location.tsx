4022a337a20262072f0187b35c593279588499c2
Amal C P <acp070146@gmail.com>
2024-02-26 23:32:34 +0530

    location page dine with structure and css

--- src/pages/location/Location.tsx
+++ src/pages/location/Location.tsx
@@ -15,13 +15,13 @@ export const Locations = (_props: Props) => {
         <iframe
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3922.7385437942385!2d76.22204117573409!3d10.521247363847355!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba7ef88b15ff21f%3A0xa8d9a1903c6873c3!2sThomson&#39;s%20Casa!5e0!3m2!1sen!2sin!4v1708967919303!5m2!1sen!2sin"
           width="100%"
-          height="450"
+          height="350"
           style={{ border: 0 }}
           loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
         <h1>What would you like to know?</h1>
-        <div>
+        <div className={styles.Container}>
           <div>
             <h3>
               Morbi accumsan ipsum velit. Nam nec tellus. Proin gravida nibh vel
